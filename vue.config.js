60179cfd860123245b01ba9a1314cb5068868c43
hocoa <1205232253@qq.com>
2020-08-08 14:05:22 +0800

    MD

--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,16 @@
-module.exports = {
+"use strict";
 
-  // pages: {
-  //   index: {
-  //     // page 的入口
-  //     entry: "src/main.js",
-  //     // 模板来源
-  //     template: "public/index.html",
-  //     // 在 dist/index.html 的输出
-  //     filename: "index.html",
-  //     // 当使用 title 选项时，
-  //     // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
-  //     title: "Index Page",
-  //     // 在这个页面中包含的块，默认情况下会包含
-  //     // 提取出来的通用 chunk 和 vendor chunk。
-  //     chunks: ["chunk-vendors", "chunk-common", "index"]
-  //   },
-  //   // 当使用只有入口的字符串格式时，
-  //   // 模板会被推导为 `public/subpage.html`
-  //   // 并且如果找不到的话，就回退到 `public/index.html`。
-  //   // 输出文件名会被推导为 `subpage.html`。
-  //   subpage: "src/main.js"
-  // }
+module.exports = {
+  // 部署生产环境和开发环境下的URL。
+  // 默认情况下，Vue CLI 会假设你的应用是被部署在一个域名的根路径上
+  // 例如 https://www.ruoyi.vip/。如果应用被部署在一个子路径上，你就需要用这个选项指定这个子路径。例如，如果你的应用被部署在 https://www.ruoyi.vip/admin/，则设置 baseUrl 为 /admin/。
+  publicPath: "./",
+  // 在npm run build 或 yarn build 时 ，生成文件的目录名称（要和baseUrl的生产环境路径一致）（默认dist）
+  outputDir: "dist",
+  // 用于放置生成的静态资源 (js、css、img、fonts) 的；（项目打包之后，静态资源会放在这个文件夹下）
+  assetsDir: "static",
+  // 是否开启eslint保存检测，有效值：ture | false | 'error'
+  lintOnSave: process.env.NODE_ENV === "development",
+  // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建。
+  productionSourceMap: false
 };
